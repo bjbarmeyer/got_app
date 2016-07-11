cf665bcee62c433536966d21e8562d084246ee3c
Britt Barmeyer <bjbarmeyer@gmail.com>
2016-07-11 00:44:49 -0700

    added hbs files for all houses. links are working

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,49 @@ router.get('/house_matcher', function(req, res, next) {
     res.render('house_matcher', {title:'GOT House Matcher'});
 });
 
+/* GET house arryn page. */
+router.get('/arryn', function(req, res, next) {
+    res.render('arryn', {title:'House Arryn'});
+});
+
+/* GET house arryn page. */
+router.get('/baratheon', function(req, res, next) {
+    res.render('baratheon', {title:'House Baratheon'});
+});
+
+/* GET house arryn page. */
+router.get('/greyjoy', function(req, res, next) {
+    res.render('greyjoy', {title:'House Greyjoy'});
+});
+
+/* GET house arryn page. */
+router.get('/lannister', function(req, res, next) {
+    res.render('lannister', {title:'House Lannister'});
+});
+
+/* GET house arryn page. */
+router.get('/martell', function(req, res, next) {
+    res.render('martell', {title:'House Martell'});
+});
+
+/* GET house arryn page. */
+router.get('/stark', function(req, res, next) {
+    res.render('stark', {title:'House Stark'});
+});
+
+/* GET house arryn page. */
+router.get('/targaryen', function(req, res, next) {
+    res.render('targaryen', {title:'House Targaryen'});
+});
+
+/* GET house arryn page. */
+router.get('/tully', function(req, res, next) {
+    res.render('tully', {title:'House Tully'});
+});
+
+/* GET house arryn page. */
+router.get('/tyrell', function(req, res, next) {
+    res.render('tyrell', {title:'House tyrell'});
+});
+
 module.exports = router;
