d28d43984c263d0d0feb1871fac7c34246b99ead
Britt Barmeyer <bjbarmeyer@gmail.com>
2016-07-12 00:05:40 -0700

    made stuff pretty. added world map page

--- routes/index.js
+++ routes/index.js
@@ -13,59 +13,100 @@ router.get('/', function(req, res, next) {
 router.get('/home', function(req, res, next) {
   //var username = req.session.user.username;
   //var isOwner = req.session.user.type.trim() == "owner";
-  res.render('home', {title: 'GOT App', /*isOwner:isOwner, username:username*/});
+  var wm_cur = false;
+  var hm_cur = false;
+  var h_cur = false;
+  res.render('home', {title: 'GOT App', wm_cur:wm_cur, hm_cur:hm_cur, h_cur:h_cur/*isOwner:isOwner, username:username*/});
+});
+
+/* GET world map page. */
+router.get('/world_map', function(req, res, next) {
+	var wm_cur = true;
+	var hm_cur = false;
+	var h_cur = false;
+    res.render('world_map', {title:'Game of Thrones World Map', wm_cur:wm_cur, hm_cur:hm_cur, h_cur:h_cur});
 });
 
 /* GET house_matcher page. */
 router.get('/house_matcher', function(req, res, next) {
     //var username = req.session.user.name;
     //var isOwner = req.session.user.role.trim() == "owner";
-    res.render('house_matcher', {title:'GOT House Matcher'});
+    var wm_cur = false;
+    var hm_cur = true;
+    var h_cur = false;
+    res.render('house_matcher', {title:'GOT House Matcher', wm_cur:wm_cur, hm_cur:hm_cur, h_cur:h_cur});
 });
 
 /* GET house arryn page. */
 router.get('/arryn', function(req, res, next) {
-    res.render('arryn', {title:'House Arryn'});
+	var wm_cur = false;
+    var hm_cur = false;
+    var h_cur = true;
+    res.render('arryn', {title:'House Arryn', wm_cur:wm_cur, hm_cur:hm_cur, h_cur:h_cur});
 });
 
 /* GET house arryn page. */
 router.get('/baratheon', function(req, res, next) {
-    res.render('baratheon', {title:'House Baratheon'});
+	var wm_cur = false;
+    var hm_cur = false;
+    var h_cur = true;
+    res.render('baratheon', {title:'House Baratheon', wm_cur:wm_cur, hm_cur:hm_cur, h_cur:h_cur});
 });
 
 /* GET house arryn page. */
 router.get('/greyjoy', function(req, res, next) {
-    res.render('greyjoy', {title:'House Greyjoy'});
+	var wm_cur = false;
+    var hm_cur = false;
+    var h_cur = true;
+    res.render('greyjoy', {title:'House Greyjoy', wm_cur:wm_cur, hm_cur:hm_cur, h_cur:h_cur});
 });
 
 /* GET house arryn page. */
 router.get('/lannister', function(req, res, next) {
-    res.render('lannister', {title:'House Lannister'});
+	var wm_cur = false;
+    var hm_cur = false;
+    var h_cur = true;
+    res.render('lannister', {title:'House Lannister', wm_cur:wm_cur, hm_cur:hm_cur, h_cur:h_cur});
 });
 
 /* GET house arryn page. */
 router.get('/martell', function(req, res, next) {
-    res.render('martell', {title:'House Martell'});
+	var wm_cur = false;
+    var hm_cur = false;
+    var h_cur = true;
+    res.render('martell', {title:'House Martell', wm_cur:wm_cur, hm_cur:hm_cur, h_cur:h_cur});
 });
 
 /* GET house arryn page. */
 router.get('/stark', function(req, res, next) {
-    res.render('stark', {title:'House Stark'});
+	var wm_cur = false;
+    var hm_cur = false;
+    var h_cur = true;
+    res.render('stark', {title:'House Stark', wm_cur:wm_cur, hm_cur:hm_cur, h_cur:h_cur});
 });
 
 /* GET house arryn page. */
 router.get('/targaryen', function(req, res, next) {
-    res.render('targaryen', {title:'House Targaryen'});
+	var wm_cur = false;
+    var hm_cur = false;
+    var h_cur = true;
+    res.render('targaryen', {title:'House Targaryen', wm_cur:wm_cur, hm_cur:hm_cur, h_cur:h_cur});
 });
 
 /* GET house arryn page. */
 router.get('/tully', function(req, res, next) {
-    res.render('tully', {title:'House Tully'});
+	var wm_cur = false;
+    var hm_cur = false;
+    var h_cur = true;
+    res.render('tully', {title:'House Tully', wm_cur:wm_cur, hm_cur:hm_cur, h_cur:h_cur});
 });
 
 /* GET house arryn page. */
 router.get('/tyrell', function(req, res, next) {
-    res.render('tyrell', {title:'House tyrell'});
+	var wm_cur = false;
+    var hm_cur = false;
+    var h_cur = true;
+    res.render('tyrell', {title:'House Tyrell', wm_cur:wm_cur, hm_cur:hm_cur, h_cur:h_cur});
 });
 
 module.exports = router;
